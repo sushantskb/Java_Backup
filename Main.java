0f39f0bc0236ab7233c55d7be5901ca02f80edde
Sushant Kumar Bishoi <91365425+sushantskb@users.noreply.github.com>
2026-03-12 14:54:18 +0000

    configured java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java is running in the cloud!");
+        System.out.println("Hello world!");
     }
 }
